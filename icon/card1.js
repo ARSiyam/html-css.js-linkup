8c6cf0c87b645514a3fcaaa89af9d6a458b2cf5c
ARSiyam <102583517+ARSiyam@users.noreply.github.com>
2022-04-06 16:04:46 +0600

    link up

---
+++ icon/card1.js
@@ -0,0 +1,20 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl1");
+    owl.owlCarousel({
+      items:3,
+      margin: 10,
+      loop: true,
+      nav: true
+    });
+  });
+  $(function() {
+    // Owl Carousel
+    var owl1 = $(".img-text");
+    owl1.owlCarousel({
+      items:2,
+      margin: 10,
+      loop: true,
+      nav: true
+    });
+  });
